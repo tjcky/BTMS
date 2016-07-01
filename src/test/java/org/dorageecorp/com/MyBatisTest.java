1f1e962ea99e2cac74598bc1ba0a9bb02389a3b9
YOUNG <tjcky@naver.com>
2016-07-01 14:39:24 +0900

    Lombok 적용 테스트

--- src/test/java/org/dorageecorp/com/MyBatisTest.java
+++ src/test/java/org/dorageecorp/com/MyBatisTest.java
@@ -10,22 +10,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
 public class MyBatisTest {
 
 	@Inject
 	private SqlSessionFactory sqlFactory;
-	
+
 	@Test
 	public void testFactory() {
 		System.out.println(sqlFactory);
 	}
-	
+
 	@Test
 	public void testSession() throws Exception {
-		try (SqlSession session = sqlFactory.openSession()){
+		try (SqlSession session = sqlFactory.openSession()) {
 			System.out.println(session);
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
