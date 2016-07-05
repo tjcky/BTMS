f9c5d5a2740801de3d2959238e65a14130d4458c
YOUNG <tjcky@naver.com>
2016-07-05 20:05:55 +0900

    Test Commit

--- src/main/java/org/dorageecorp/com/board/controller/HomeController.java
+++ src/main/java/org/dorageecorp/com/board/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Test ∆‰¿Ã¡ˆ
+ * Test
  */
 @Controller
 public class HomeController {
