cfa532767b404ed9d41172bbe619c42683144b23
YOUNG <tjcky@naver.com>
2016-07-20 21:31:53 +0900

    알파 테스트 위한 호스트 임시변경

--- src/main/webapp/resources/javascript/main.js
+++ src/main/webapp/resources/javascript/main.js
@@ -1,34 +1,43 @@
+redirect = "http://10.113.182.210:8080";
+
 function goToMain() {
 	url = "/main";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
 
 function goToService() {
 	url = "/kcmsus";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
 
 function goToActivitySchedule() {
 	url = "/kcmsus/activitySchedule";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
 
 function goToActivityList() {
 	url = "/kcmsus/activityList";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
 
 function goToJobTemplateList() {
 	url = "/kcmsus/jobTemplateList";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
 
 function goToJobDetail() {
 	url = "/kcmsus/jobDetail";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
 
 function goToJobVersionList() {
 	url = "/kcmsus/jobVersionList";
-	location.href = url;
+	/*location.href = url;*/
+	window.location.href = redirect + url;
 }
